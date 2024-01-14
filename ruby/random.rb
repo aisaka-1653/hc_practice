a60ef44e0b67ea067d3d051195509624e3591721
aisaka-1653 <reobata1127@gmail.com>
2024-01-14 15:28:36 +0900

    Ruby グループ分け課題のPR

--- ruby/random.rb
+++ ruby/random.rb
@@ -1,8 +1,11 @@
+# frozen_string_literal: true
+
 def shuffle_group
-  group = [*'A'..'F'].shuffle
-  ratio = [2, 3].sample
-  a = p group[ratio..]
-  p group - a
+  shuffled_group = [*'A'..'F'].shuffle
+  # 3:3か4:2に分ける
+  ratio = [3, 4].sample
+  sliced_groupes = shuffled_group.each_slice(ratio)
+  sliced_groupes.each { |group| p group.sort }
 end
 
 shuffle_group
